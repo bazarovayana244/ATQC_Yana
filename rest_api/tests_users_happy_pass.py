a5f69cb80dc5c63768e61cb92f2604944b04b580
Yana_Bazarova <bazarovayana244@gmail.com>
2025-10-29 19:14:40 +0000

    Changes to HappyFlow

--- rest_api/tests_users_happy_pass.py
+++ rest_api/tests_users_happy_pass.py
@@ -1,33 +1,32 @@
 from users_happy_pass import UserAPI
 
 class TestUserAPI:
-
-    def test_get_users_happy_pass(self):
+    def test_user_management_happy_flow(self):
+        #GET
         response = UserAPI.get_users()
         assert response.status_code == 200
         data = response.json()
         assert isinstance(data, dict)
         assert len(data) > 0
 
-    def test_post_user_happy_pass(self):
+        #POST
         payload = {"name": "Charlie", "age": 28}
         response = UserAPI.create_user(payload)
         assert response.status_code == 201
         data = response.json()
         assert "id" in data
+        user_id = data["id"]
         assert data["user"]["name"] == "Charlie"
         assert data["user"]["age"] == 28
 
-    def test_put_user_happy_pass(self):
-        user_id = 3
-        payload = {"age": 29}
-        response = UserAPI.update_user(user_id, payload)
+        #PUT
+        update_payload = {"age": 29}
+        response = UserAPI.update_user(user_id, update_payload)
         assert response.status_code == 200
         data = response.json()
         assert data["age"] == 29
 
-    def test_delete_user_happy_flow(self):
-        user_id = 3
+        #DELETE
         response = UserAPI.delete_user(user_id)
         assert response.status_code == 200
         data = response.json()
