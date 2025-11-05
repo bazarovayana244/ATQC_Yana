f081927065d28f5952504cf74bcb9b4ddaaca9de
Yana_Bazarova <bazarovayana244@gmail.com>
2025-11-05 20:21:38 +0000

    Update conftest for headless mode

--- test_execution/conftest.py
+++ test_execution/conftest.py
@@ -1,14 +1,21 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", action="store", default="true",
+        help="Run browser in headless mode: true or false"
+    )
+
 @pytest.fixture(scope="session")
 def playwright_instance():
     with sync_playwright() as p:
         yield p
 
 @pytest.fixture(scope="session")
-def browser(playwright_instance):
-    browser = playwright_instance.chromium.launch(headless=False)
+def browser(pytestconfig, playwright_instance):
+    headless_option = pytestconfig.getoption("--headless").lower() == "true"
+    browser = playwright_instance.chromium.launch(headless=headless_option)
     yield browser
     browser.close()
 
@@ -19,3 +26,4 @@ def page(browser):
     yield page
     page.close()
     context.close()
+
